87b7d2026e909830cc45e0d76d88d46fce7587f5
msgal97 <klyoung2@mail.bradley.edu>
2015-11-12 09:54:27 -0600

    updating

--- wiki.php
+++ wiki.php
@@ -10,18 +10,23 @@
 
 <?php
 
-if (isset($_GET['content'])) {
-    $content = $_GET['content'];
-    file_put_contents('wiki.txt', $content);
-
 }
 if (file_exists('wiki.txt')) {
     $content = file_get_contents('wiki.txt');
 } else {
-    $content = '(foo)';
+    $content = '(no content)';
 }
-$safe_content = htmlentities($content);
-//echo $safe_content;
+if(file_exists('wiki.txt')){
+  $wiki = file_get_contents('wiki.txt');
+}
+else{
+  $wiki = 'no wiki';
+}
+if(isset($_GET['content'])){
+  $content = $_GET['content'];
+    file_put_contents('wiki.txt', $content);
+}
+
 ?>
 
 <div id="content">
