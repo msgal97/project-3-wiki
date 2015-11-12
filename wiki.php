87149305a67b22a65e11393d04d296c2ed77674b
msgal97 <klyoung2@mail.bradley.edu>
2015-11-11 21:24:32 -0600

    trial

--- wiki.php
+++ wiki.php
@@ -3,11 +3,11 @@ if (isset($_GET['content'])) {
     $content = $_GET['content'];
     file_put_contents('wiki.txt', $content);
 }
-if (file_exists('wiki.txt')) {
-    $content = file_get_contents('wiki.txt');
-} else {
-    $content = '(no content)';
-}
+//if (file_exists('wiki.txt')) {
+  //  $content = file_get_contents('wiki.txt');
+//} else {
+  //  $content = '(no content)';
+//}
 
 $safe_content = htmlentities($content);
 echo $safe_content;
