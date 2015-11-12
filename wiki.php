9879d76eeafe11ed9792f6f51307391ebd8bf20b
msgal97 <klyoung2@mail.bradley.edu>
2015-11-11 21:30:21 -0600

    foo

--- wiki.php
+++ wiki.php
@@ -2,13 +2,13 @@
 if (isset($_GET['content'])) {
     $content = $_GET['content'];
     file_put_contents('wiki.txt', $content);
-    $safe_content = htmlentities($content);
-    echo $safe_content;
+
 }
 if (file_exists('wiki.txt')) {
     $content = file_get_contents('wiki.txt');
 } else {
-    $content = '(no content)';
+    $content = '(foo)';
 }
-
+$safe_content = htmlentities($content);
+echo $safe_content;
 ?>
