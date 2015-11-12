aac6dbf51056d245eb73f5d8e387b28ddee9f7dd
msgal97 <klyoung2@mail.bradley.edu>
2015-11-11 21:22:13 -0600

    added data

--- wiki.php
+++ wiki.php
@@ -1,14 +1,14 @@
 <?php
-
+if (isset($_GET['content'])) {
+    $content = $_GET['content'];
+    file_put_contents('wiki.txt', $content);
+}
 if (file_exists('wiki.txt')) {
     $content = file_get_contents('wiki.txt');
 } else {
     $content = '(no content)';
 }
-if (isset($_GET['content'])) {
-    $content = $_GET['content'];
-    file_put_contents('wiki.txt', $content);
-}
+
 $safe_content = htmlentities($content);
 echo $safe_content;
 ?>
