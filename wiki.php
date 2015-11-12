0bccac475317f950de8a7eb50676bd8c49c0691d
msgal97 <klyoung2@mail.bradley.edu>
2015-11-11 21:15:04 -0600

    writing data

--- wiki.php
+++ wiki.php
@@ -5,6 +5,10 @@ if (file_exists('wiki.txt')) {
 } else {
     $content = '(no content)';
 }
+if (isset($_GET['content'])) {
+    $content = $_GET['content'];
+    file_put_contents('wiki.txt', $content);
+}
 $safe_content = htmlentities($content);
 echo $safe_content;
 ?>
